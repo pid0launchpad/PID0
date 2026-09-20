7d8a1191d2fbd0f5009b4c40e94efee1b3a3aca6
sammiratidata <zaunmanx@gmail.com>
2026-09-20 10:07:02 +0800

    feat: adopt ZUNON phosphor identity

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './terminal.css'
 import './product.css'
 import './polish.css'
 import './triple.css'
+import './zunon-theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
